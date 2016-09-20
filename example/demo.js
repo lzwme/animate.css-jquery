8573fbefe1a81447feedd72ab985a97ec676ac21
任侠 <lzwy0820@qq.com>
2016-09-21 02:07:34 +0800

    update example

--- example/demo.js
+++ example/demo.js
@@ -1,24 +1,33 @@
 // demo.js
 
-$(function(animatecssjs) {
+$(function (animatecssjs) {
     var $select = $('#animateList'),
         $demo = $('#demo'),
         $log = $('#logs');
 
     function log(msg) {
         console.log(msg);
-        $log.prepend('<p>' + msg + '</p>'); //.scrollTop($log.height());
+        $log.prepend('<p>' + msg + '</p>'); // .scrollTop($log.height());
     }
 
     function animateDemo(el, type, opts) {
         var colors = ['#08f', '#f80', '#af0', '#f00', '#ff0', '#0ff', '#f0f', '#000', '#4a159f', '#170'];
-        log('animate start: ' + (type || 'random'));
+        var starttime;
+
+        if (!opts || !opts.nolog) {
+            log('animate start: ' + (type || 'random'));
+        }
+
+        el = el || $demo;
         $(el).html(type || 'random').css('background', colors[Math.floor(Math.random() * colors.length)]);
 
-        var starttime = new Date();
-        return animatecssjs($.extend({$el: el, type: type}, opts)).then(function(_type) {
+        starttime = new Date();
+        return animatecssjs($.extend({$el: el, type: type}, opts)).then(function (_type) {
             $(el).html(_type);
-            log('<hr> animate end [' + (new Date() - starttime) + 'ms] : ' + _type);
+
+            if (!opts || !opts.nolog) {
+                log('<hr> animate end [' + (new Date() - starttime) + 'ms] : <button class="btn btn-xs btn-info">' + _type + '</button>');
+            }
         });
     }
 
@@ -36,31 +45,31 @@ $(function(animatecssjs) {
     }
 
     function initEvents() {
-        $select.on('change', function() {
+        $select.on('change', function () {
             var type = $(this).val();
 
             animateDemo('#demo', type);
         });
 
-        $('.infinite').on('click', function() {
+        $('.infinite').on('click', function () {
             animateDemo('#demo', '', {
                 infinite: true
             });
         });
 
-        $('.k_in').on('click', function() {
+        $('.k_in').on('click', function () {
             animateDemo('#demo', '', {
                 keyword: 'in'
             });
         });
 
-        $('.k_out').on('click', function() {
+        $('.k_out').on('click', function () {
             animateDemo('#demo', '', {
                 keyword: /out/i
             });
         });
 
-        $('.random_test').on('click', function() {
+        $('.random_test').on('click', function () {
             var $this = $(this);
 
             if ($this.hasClass('stop')) {
@@ -68,27 +77,37 @@ $(function(animatecssjs) {
             } else {
                 $this.addClass('btn-danger');
 
-                animateDemo('#demo', '').then(function() {
+                animateDemo('#demo', '').then(function () {
                     $this.click();
                 });
             }
         });
 
-        $('.random_test_stop').on('click', function() {
+        $('.random_test_stop').on('click', function () {
             $('.random_test').addClass('stop');
-        })
+        });
+
+        $demo.on('click', function () {
+            animateDemo($demo, $(this).text());
+        });
+
+        $log.on('click', 'button', function () {
+            animateDemo($demo, $(this).text(), {
+                nolog: true
+            });
+        });
 
     }
 
     function init() {
         // animate body
-        animatecssjs({$el: 'body'}).then(function(type) {
+        animatecssjs({$el: 'body'}).then(function (type) {
             log('animate body: ' + type);
 
             return $('.btn').animateCss({
                 keyword: /in/i
             });
-        }).then(function(type) {
+        }).then(function (type) {
             log('<hr> animate btns: ' + type);
         });
 
